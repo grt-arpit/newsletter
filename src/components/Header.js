f8dcc1a0e3603cffc96122b2e262fa23494f91b5
Arpit Yadav <yadavarpit927@gmail.com>
2022-07-05 13:23:30 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <a className="navbar-brand mt-2 mt-lg-0" href="#">
+              <img
+                src="https://i.pinimg.com/originals/e5/3b/f0/e53bf02c0e739b9f60a6fef132599ad3.jpg"
+                height="35"
+                alt="MDB Logo"
+                loading="lazy"
+              />
+            </a>
+
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              
+              <li className="nav-item">
+                <NavLink className="nav-link" to="signup">
+                 SignUp
+                </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink className="nav-link" to="login">
+                 LOGIN
+                </NavLink>
+              </li>
+              
+            </ul>
+          </div>
+
+         
+           
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Signup.js
@@ -0,0 +1,122 @@
+import { Formik } from "formik";
+import React from "react";
+import Swal from "sweetalert2";
+import * as Yup from "yup";
+
+const Signup = () => {
+
+  const handleFormSubmit=(formdata)=>{
+    console.log(formdata);
+
+    fetch("http://localhost:5000/user/news",{  //1. address
+      method:"POST",                           // 2.request method
+      body: JSON.stringify(formdata),          //3.data tobe sent
+      headers: {                               //4.data formate
+        "Content-Type": "application/json",
+      }
+    }).then((res) => {
+      if (res.status===200) {
+        Swal.fire({
+          icon: "success",
+          title: "Success",
+          text: "Registration successful",
+        });
+      }else{
+        Swal.fire({
+          icon: "error",
+          title: "Oops",
+          text: "some error occured",
+        });
+      }
+    })
+  }
+
+  const loginSchema = Yup.object().shape({
+    username: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .min(4, "Password should be longer than 4 characters")
+      .required("Required"),
+    });
+ 
+  return (
+    <div className="container mt-5">
+      <div className="card">
+        <div className="card-body">
+          <h3 className="text-muted text-center">SIGN UP</h3>
+          <Formik
+            initialValues={{
+              username: "",
+              email: "",
+              password: "",
+              websiteName:"",
+              createdAt:"",
+            }}
+            onSubmit={handleFormSubmit}
+            validationSchema={loginSchema}
+          >
+            {({ values,handleChange,handleSubmit,errors,touched}) => (
+              <form onSubmit={handleSubmit}>
+                <label className="mt-4">Username</label>
+                <input
+                  className="form-control"
+                  placeholder="Userame"
+                  value={values.username}
+                  id="username"
+                  onChange={handleChange}
+                  error={Boolean(errors.username)}
+                  helperText={errors.username }
+                />
+                <label className="mt-4">Email</label>
+                <input
+                  className="form-control"
+                  placeholder="email"
+                  value={values.email}
+                  id="email"
+                  onChange={handleChange}
+                  error={Boolean(errors.email) && touched.email}
+                  helperText={touched.email ? errors.email : ""}
+                />
+                <label className="mt-4">password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  placeholder="password"
+                  value={values.password}
+                  id="password"
+                  onChange={handleChange}
+                  error={Boolean(errors.password)}
+                      helperText={errors.password }
+                />
+                <label className="mt-4">websiteName</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="websiteName"
+                  value={values.websiteName}
+                  id="websiteName"
+                  onChange={handleChange}
+                />
+                <label className="mt-4">createdAt</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="createdAt"
+                  value={values.createdAt}
+                  id="createdAt"
+                  onChange={handleChange}
+                />
+
+                <button className="btn btn-primary mt-5" type="Submit">
+                  Submit
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
